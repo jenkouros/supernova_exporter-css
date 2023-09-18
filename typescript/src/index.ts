0d06b516d6d1d56883e788f560912510dcfeece0
jenkouros <jenko.uros@gmail.com>
2023-09-18 10:32:58 +0200

    Update index.ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -8,11 +8,10 @@
     
     const segments = [...tokenGroup.path];
     if (!tokenGroup.isRoot && token.tokenType !== 'Typography' && token.tokenType !== 'Gradient') {
-     segments.push('DD');
-     segments.push(token.tokenType);
-     segments.push('RR');
+
       segments.push(tokenGroup.name)
     }
+        segments.push('DD');
     segments.push(token.name);
 
     if (prefix && prefix.length > 0) {
